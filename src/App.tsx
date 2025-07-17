b281b26343d0f5b61cb83df90b355b89f979e58c
Nilabhro29 <122674283+PALLAV06@users.noreply.github.com>
2025-07-17 08:55:52 +0530

    added speed insights and analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Home from "./components/home";
 import routes from "tempo-routes";
 
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
         {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
+        <Analytics />
+        <SpeedInsights />
       </>
     </Suspense>
   );
